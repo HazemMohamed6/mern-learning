19d6dc78d8f7835671af72edab0904078ecc35b5
Hazem Mohamed <hazemmohameddeshesha@gmail.com>
2026-03-09 07:26:46 +0200

    first javascript practice

---
+++ script.js
@@ -0,0 +1,13 @@
+console.log ("hello hazem")
+let name ="Hazem"
+let age = 23
+console.log("my name is "+ name)
+console.log("my age is "+ age)
+if (age > 18){
+    console.log("Adult")
+}else{
+    console.log("Minor")
+}
+for(let i;i<=5;i++){
+    console.log(i)
+}
